ef691b69fab24cf006e4df2f45bacdeb890eff80
Arjan Villon <heaven.arjan@gmail.com>
2023-08-29 00:35:41 +0800

    chore: reverse order of websocket urls

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ const { isProduction } = require("./utils");
 module.exports = {
   USER_IDS: process.env.USER_IDS.split(","),
   WEBSOCKET_URLS: isProduction()
-    ? ["wss://proxy.wynd.network:4444", "wss://proxy.wynd.network:4650"]
+    ? ["wss://proxy.wynd.network:4650", "wss://proxy.wynd.network:4444"]
     : ["ws://proxy.dev.getgrass.io:4343"],
   NAMESPACE: "bfeb71b6-06b8-5e07-87b2-c461c20d9ff6",
   PING_INTERVAL: 20 * 1000, // 20s in ms
