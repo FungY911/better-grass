bd5f58e340b7e802fe444cbbd8c608321f675979
FungY911 <fungy539@gmail.com>
2024-03-10 12:32:24 +0100

    Fixed updating repo

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,13 +2,21 @@
 
 LOCAL_DIR=$(pwd)
 GITHUB_REPO=https://github.com/FungY911/better-grass
-APP_NAME=grass-cli
+APP_NAME=better-grass
 
 # Make sure to change directory to the working app
 cd "$LOCAL_DIR" || exit
 
+# Clone the repository if not already present
+if [ ! -d "$APP_NAME" ]; then
+    git clone "$GITHUB_REPO" "$APP_NAME"
+fi
+
+# Change directory to the app
+cd "$APP_NAME" || exit
+
 # Pull from the main branch of the app
 git pull origin main
 
-# Update dependencies if necessary
+# Update dependencies
 npm install
