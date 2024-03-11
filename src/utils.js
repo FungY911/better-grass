5162b585c7baa11416243e6a43e1fc3eaabb870e
FungY911 <fungy539@gmail.com>
2024-03-11 16:40:22 +0100

    Optimized utils

--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,17 @@
-const isProduction = () => {
-  return process.env.NODE_ENV === "production";
-};
+const isProduction = () => process.env.NODE_ENV === "production";
 
 const prettifyHeaderKey = (headerKey) => {
   const HEADER_KEY_EXCEPTIONS = [
-    "CH",
-    "CT",
-    "DNS",
-    "DNT",
-    "DPR",
-    "ECT",
-    "ETag",
-    "GPC",
-    "NEL",
-    "RTT",
-    "SourceMap",
-    "TE",
-    "UA",
-    "WWW",
-    "WebSocket",
-    "XSS",
+    "CH", "CT", "DNS", "DNT", "DPR", "ECT", "ETag", "GPC", "NEL", "RTT",
+    "SourceMap", "TE", "UA", "WWW", "WebSocket", "XSS"
   ];
 
-  const result = [];
-  const parts = headerKey.split("-");
-  for (const part of parts) {
+  return headerKey.split("-").map(part => {
     const nameException = HEADER_KEY_EXCEPTIONS.find(
       (el) => el.toLowerCase() === part.toLowerCase()
     );
-    if (nameException !== undefined) {
-      result.push(nameException);
-    } else {
-      // capitalize the first letter
-      result.push(part.charAt(0).toUpperCase() + part.slice(1));
-    }
-  }
-
-  return result.join("-");
+    return nameException !== undefined ? nameException : part.charAt(0).toUpperCase() + part.slice(1);
+  }).join("-");
 };
 
-module.exports = {
-  isProduction,
-  prettifyHeaderKey,
-};
+module.exports = { isProduction, prettifyHeaderKey };
