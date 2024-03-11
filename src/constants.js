f7ed35ac803ff2883e53c794a7f50c143c1d645f
FungY911 <fungy539@gmail.com>
2024-03-11 16:42:20 +0100

    Check if there is provided a user_id.

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,16 @@
 const { isProduction } = require("./utils");
 
+let userIds = process.env.USER_ID !== undefined && process.env.USER_IDS !== "" ? process.env.USER_IDS.split(",") : [];
+// If USER_IDS is not defined or empty, an empty array will be used
+
+if (userIds.length === 0) {
+  console.log("[❌] No user IDs provided!");
+  console.log("[❗] Please set the USER_IDS environment variable.");
+  process.exit(1);
+}
+
 module.exports = {
-  USER_IDS: process.env.USER_IDS.split(","),
+  USER_IDS: userIds,
   WEBSOCKET_URLS: isProduction()
     ? ["wss://proxy.wynd.network:4650", "wss://proxy.wynd.network:4444"]
     : ["ws://proxy.dev.getgrass.io:4343"],
